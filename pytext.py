1ae4490ced64a35b4354b45dcba8011a91acff8c
David Snider <dsnider.techie@gmail.com>
2016-11-14 20:07:25 -0600

    v0.2
    it's working!!!
    can send text to self!
    MAJOR EXCITEMENT!!!!!!!

--- pytext.py
+++ pytext.py
@@ -3,3 +3,15 @@
 # November 14,2016
 
 '''an attempt at using python to send texts'''
+from twilio.rest import TwilioRestClient
+accountSID = 'AC1ce4d981192db20123f84bd6cf6035bc'
+authToken = '643726e40cc3def605b4e72c46c6ae5f'
+
+twilioCli = TwilioRestClient(accountSID,authToken)
+twilioNumber = '+13146268235'
+myNumber = '+16362321563'
+
+
+#testing
+#twilioCli.messages.create(body='testing testing \
+#1234 1234', from_=twilioNumber, to=myNumber)
